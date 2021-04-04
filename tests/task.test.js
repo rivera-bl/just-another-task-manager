3c5bc2a3e3e9dbabba72f189fb7b0b5a8d5a1e3a
rivera-bl <rivera.blo@protonmail.com>
2021-04-04 11:01:50 -0400

    added Test Case for Creating a Task

--- tests/task.test.js
+++ tests/task.test.js
@@ -7,6 +7,16 @@ const { userLog, userLogId, setupDatabase } = require('./fixtures/db.js')
 // beforeEach will run before each test case
 beforeEach(setupDatabase)
 
-test('Should create task for user', () => {
-    
+test('Should create task for user', async () => {
+    const res = await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userLog.tokens[0].token}`)
+        .send({
+            description: 'Test Task'
+        })
+        .expect(201)
+
+    const task = await Task.findById(res.body._id)
+    expect(task).not.toBeNull()
+    expect(task.completed).toEqual(false)
 })
