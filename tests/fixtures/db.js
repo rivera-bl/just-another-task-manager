bce2f400b03cbf0832eeefb1865a11d9df6b9347
rivera-bl <rivera.blo@protonmail.com>
2021-04-04 11:29:37 -0400

    added Test Cases for GET all Tasks and DELETE other user Task

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const User = require('../../src/models/user.js')
+const Task = require('../../src/models/task.js')
 
 // creates an id for the user
 const userLogId = new mongoose.Types.ObjectId()
-
 const userLog = {
     _id: userLogId,
     name: 'Test Login',
@@ -15,15 +15,54 @@ const userLog = {
     }]
 }
 
+const userTaskId = new mongoose.Types.ObjectId()
+const userTask = {
+    _id: userTaskId,
+    name: 'Test User Task',
+    email: 'testusertask@example.com',
+    password: 'pass12345',
+    tokens: [{
+        token: jwt.sign({ _id: userTaskId }, process.env.JWT_SECRET)
+    }]
+}
+
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'First Test Description',
+    default: false,
+    owner: userLog._id
+}
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Second Test Description',
+    default: true,
+    owner: userLog._id
+}
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Third Test Description',
+    default: true,
+    owner: userTask._id
+}
+
 const setupDatabase = async () => {
-    // deletes every user of the database
+    // deletes every user and task of the database
     await User.deleteMany()
-    // creates a test user for cases that require an user logged in
+    await Task.deleteMany()
     await new User(userLog).save()
+    await new User(userTask).save()
+    await new Task(taskOne).save()
+    await new Task(taskTwo).save()
+    await new Task(taskThree).save()
 }
 
 module.exports = {
     userLog,
     userLogId,
+    userTask,
+    userTaskId,
+    taskOne,
     setupDatabase
 }
--- tests/task.test.js
+++ tests/task.test.js
@@ -1,12 +1,13 @@
 const request = require('supertest')
 const app = require('../src/app.js')
 const Task = require('../src/models/task.js')
-const { userLog, userLogId, setupDatabase } = require('./fixtures/db.js')
+const { userLog, userLogId, userTask, userTaskId, taskOne, setupDatabase } = require('./fixtures/db.js')
 
 // jest setup & teardown for lifecycle functions
 // beforeEach will run before each test case
 beforeEach(setupDatabase)
 
+// CREATE TASK
 test('Should create task for user', async () => {
     const res = await request(app)
         .post('/tasks')
@@ -19,4 +20,24 @@ test('Should create task for user', async () => {
     const task = await Task.findById(res.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)
+})
+
+// GET ALL TASKS
+test('Should get all the tasks of an user', async () => {
+    const res = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userLog.tokens[0].token}`)
+        .expect(200)
+
+    expect(res.body.length).toEqual(2)
+})
+
+// DELETE A TASK FROM ANOTHER USER
+test('Should not delete tasks from other user', async () => {
+    const res = await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTask.tokens[0].token}`)
+        .expect(404)
+
+    expect(taskOne).not.toBeUndefined()
 })
